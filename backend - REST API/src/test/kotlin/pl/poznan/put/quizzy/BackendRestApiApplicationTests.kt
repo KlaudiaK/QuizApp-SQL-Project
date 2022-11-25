afcbd966dc312d3d4feca428a5b3e654a386f277
Michał <michauz2001@gmail.com>
2022-11-25 08:43:44 +0100

    Initilize backend service classes

---
+++ backend - REST API/src/test/kotlin/pl/poznan/put/quizzy/BackendRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.poznan.put.quizzy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
