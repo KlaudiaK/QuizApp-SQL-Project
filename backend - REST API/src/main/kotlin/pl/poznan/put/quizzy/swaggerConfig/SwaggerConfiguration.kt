88801776b64fa5f82f0b286bf5235b8fc5a87538
Michał <michauz2001@gmail.com>
2022-12-17 11:22:42 +0100

    Swagger configuration

--- backend - REST API/src/main/kotlin/pl/poznan/put/quizzy/swaggerConfig/SwaggerConfiguration.kt
+++ backend - REST API/src/main/kotlin/pl/poznan/put/quizzy/swaggerConfig/SwaggerConfiguration.kt
@@ -2,8 +2,10 @@ package pl.poznan.put.quizzy.swaggerConfig
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -16,7 +18,17 @@ class SwaggerConfiguration{
         return Docket(DocumentationType.SWAGGER_2)
             .select()
             .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
+            .paths(PathSelectors.ant("/api/**"))
+            .build()
+            .apiInfo(getApiInfo())
+    }
+
+    private fun getApiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+            .title("QUIZZY - REST API")
+            .description("Quizzy rest api documentation")
+            .version("1.0.0")
             .build()
     }
+
 }
